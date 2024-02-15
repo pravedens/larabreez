b7d3be8f63710f5d9b6e6d4cecf3fe349e60c4df
pravedens <pravedens@gmail.com>
2024-02-15 18:01:06 +0500

    first commit

---
+++ resources/views/emails/template_email.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>Test Email</h1>
+    <p>
+        Just Sending my email.........
+        <br>
+        new Content from user:
+        {{ $mailData['content'] }}
+    </p>
+</body>
+</html>
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Jobs\SendEmailJob;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('send-email', function(Request $request){
+
+    try{
+        $data['email'] = $request->email;
+        $data['content'] = $request->content;
+
+        dispatch(new SendEmailJob($data));
+
+        return response()->json(['message' => 'berhasil dikirim']);
+    }catch(Exception $e){
+        return response()->json(['message' => 'error '.$e->getMessage()]);
+    }
+
+});
